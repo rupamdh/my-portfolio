6114f225c9e940a441c4f6fe1707165a7b0ebec2
Rupam Dhara <thedevgenius83@gmail.com>
2024-01-12 16:55:46 +0530

    Update

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:2g16E1d1e25e52C55GcaG5CFE32CFddg@viaduct.proxy.rlwy.net:46498/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '2g16E1d1e25e52C55GcaG5CFE32CFddg',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': 46498,
     }
 }
 
